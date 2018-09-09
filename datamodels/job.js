753f31b162467c6ca23ac9a87c30b0504cce4cd0
kelum <edirisingheemks@gmail.com>
2018-09-09 12:14:50 +0530

    job post completed

--- datamodels/job.js
+++ datamodels/job.js
@@ -3,19 +3,24 @@ const shortid = require('shortid');
 var mydate = require('current-date');
 
 module.exports.jobsave=function(job,callback){
-    console.log(job);
+    //console.log(job);
    // callback(null,true);
   if(dbconnection.connection){ 
-     dbconnection.connection.query('INSERT INTO jobrequestpost (postid,customerid,dateandtime,status) VALUES (?,?,?,?)', [shortid.generate(),job.customerid,mydate('full', '-', ':'),job.status],function (err, rows, fields) {
+      const postid = shortid.generate();
+     dbconnection.connection.query('INSERT INTO jobrequestpost (postid,customerid,dateandtime,status) VALUES (?,?,?,?)', [postid,job.customerid,mydate('full', '-', ':'),job.status],function (err, rows, fields) {
          if (err){
              callback(err);
          }else{
-             //dbconnection.connection.end();
-             //console.log(rows[0]);
-             callback(null,rows[0]);
+            dbconnection.connection.query('INSERT INTO description (descriptionid,postid,title,employeegender,priceperhour,estimatedtime,joblocation,jobdate) VALUES (?,?,?,?,?,?,?,?)', [shortid.generate(),postid,job.title,job.gender,job.priceperhour,job.estimatedtime,job.joblocation,job.jobdate],function (err, rows, fields) {
+                if (err){
+                    callback(err);
+                }else{
+                    //dbconnection.connection.end();
+                    //console.log(rows[0]);
+                    callback(null,rows[0]);
+                }
+              })  
          }
-       
-         
        })  
     }else{
         callback(err);
