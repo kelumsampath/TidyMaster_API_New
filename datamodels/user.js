260491605b653ecaff45218298f1ae96057e410e
Randula_u <randulakoralage82@gmail.com>
2018-10-23 20:17:01 +0530

    remove comments

--- datamodels/user.js
+++ datamodels/user.js
@@ -1,86 +1,61 @@
-//const mongoose = require('mongoose');
+
 const bcrypt = require('bcryptjs');
 const dbconnection = require('./dbconnect');
 const shortid = require('shortid');
 var mydate = require('current-date');
-//const schema = mongoose.Schema;
 
 
-/*const userSchema = new schema({
-    fullname:{type:String,required:true},
-    username:{type:String,required:true, unique:true},
-    email:{type:String,required:true},
-    phoneno:{type:Number,required:true},
-    password:{type:String,required:true}
 
-});
 
-const datamodels = module.exports = mongoose.model("datamodels",userSchema);
-*/
-module.exports.dbSave = function(regUser,callback){
 
-        bcrypt.genSalt(10, function(err, salt) {
-            bcrypt.hash(regUser.password, salt, function(err, hash) {
-                //console.log(hash);
-                regUser.password = hash;
-                if(err){
-                    throw err;
-                }else{
-                   // regUser.save(err, callback);
-                   if(dbconnection.connection){ 
-                    dbconnection.connection.query('call addCustomerOrCleaner(?,?,?,?,?,?,?,?,?,?,?,?,?)', [shortid.generate(),regUser.role, regUser.username, regUser.password, regUser.email , regUser.nic, regUser.photoId, regUser.telephone , regUser.firstname, regUser.lastname, regUser.gender, regUser.address, shortid.generate()],function (err, rows, fields) {
-                        if (err){
+module.exports.dbSave = function (regUser, callback) {
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(regUser.password, salt, function (err, hash) {
+            regUser.password = hash;
+            if (err) {
+                throw err;
+            } else {
+                if (dbconnection.connection) {
+                    dbconnection.connection.query('call addCustomerOrCleaner(?,?,?,?,?,?,?,?,?,?,?,?,?)', [shortid.generate(), regUser.role, regUser.username, regUser.password, regUser.email, regUser.nic, regUser.photoId, regUser.telephone, regUser.firstname, regUser.lastname, regUser.gender, regUser.address, shortid.generate()], function (err, rows, fields) {
+                        if (err) {
                             callback(err);
-                        }else{
-                            //dbconnection.connection.end();
-                            //console.log('The solution is: ');
-                            callback(null,true);
+                        } else {
+                            callback(null, true);
                         }
-                      
-                        
-                      })  
-                   }else{
-                       callback(err);
-                   }
+                    })
+                } else {
+                    callback(err);
                 }
-            });
+            }
         });
-
+    });
 };
 
 
-module.exports.searchUser = function(username,callback){
-   // const query = {username:username};
-   // datamodels.findOne(query,callback);
-   if(dbconnection.connection){ 
-    dbconnection.connection.query('SELECT * FROM user WHERE username=?', [username],function (err, rows, fields) {
-        if (err){
-            callback(err);
-        }else{
-            //dbconnection.connection.end();
-            //console.log(rows[0]);
-            callback(null,rows[0]);
+module.exports.searchUser = function (username, callback) {
+    if (dbconnection.connection) {
+        dbconnection.connection.query('SELECT * FROM user WHERE username=?', [username], function (err, rows, fields) {
+            if (err) {
+                callback(err);
+            } else {
+                callback(null, rows[0]);
+            }
+        })
+    } else {
+        callback(err);
+    }
+};
+
+
+
+module.exports.matchpassword = function (password, hash, callback) {
+    bcrypt.compare(password, hash, function (err, res) {
+        if (err) throw err;
+        if (res) {
+            callback(null, res);
+        } else {
+            callback(null, res);
         }
-      
-        
-      })  
-   }else{
-       callback(err);
-   }
-}; 
-
-
-
-module.exports.matchpassword = function(password,hash,callback){
-    //console.log(password+" "+hash);
-    bcrypt.compare(password, hash, function(err, res) {
-        if(err) throw  err;
-        if (res){
-            callback(null,res);
-        } else{
-            callback(null,res);
-        }
-       // console.log(res);
     });
 }
 
