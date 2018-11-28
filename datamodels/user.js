07b0cd3d2648a1e51cf162125e440a4b5c6e83bc
kelum <edirisingheemks@gmail.com>
2018-11-28 23:11:14 +0530

    search user

--- datamodels/user.js
+++ datamodels/user.js
@@ -53,6 +53,27 @@ module.exports.searchUser = function(username,callback){
 }; 
 
 
+module.exports.searchwebusers = function(username,callback){
+    // const query = {username:username};
+    // datamodels.findOne(query,callback);
+    if(dbconnection.connection){ 
+     dbconnection.connection.query('SELECT * FROM user u, role r WHERE u.roleid=r.roleid AND username LIKE "'+username+'%"', [username],function (err, rows, fields) {
+         if (err){
+             callback(err);
+         }else{
+             //dbconnection.connection.end();
+             console.log(rows);
+             callback(null,rows);
+         }
+       
+         
+       })  
+    }else{
+        callback(err);
+    }
+ }; 
+ 
+
 
 module.exports.matchpassword = function (password, hash, callback) {
     bcrypt.compare(password, hash, function (err, res) {
